99146c8b58ce75cec7e22703e1e9d0736c96d0ac
J-0-d-a-1 <[jumpei.oda.yrv@gmail.com protected]>
2025-02-27 17:04:38 -0800

    fetchBreedDescription func modified with mocha and chai test cases

--- breedFetcher.js
+++ breedFetcher.js
@@ -1,24 +1,45 @@
 const needle = require("needle");
 
-const arr = process.argv.slice(2);
+const fetchBreedDescription = function (breedName, callback) {
+  const url = `https://api.thecatapi.com/v1/breeds/search?q=${breedName.slice(
+    0,
+    4
+  )}`;
 
-const url = `https://api.thecatapi.com/v1/breeds/search?q=${arr[0].slice(
-  0,
-  4
-)}`;
-
-const getContent = (url) => {
   needle.get(url, (error, respond, body) => {
     if (!respond) {
-      console.error("Request Failed", error);
+      // callback(error);
+      callback("Request Faild");
       return;
     } else if (body.length === 0) {
-      console.error("Breed Not Found!!!", error);
+      // callback(error);
+      callback("Breed Not Found");
       return;
     } else {
-      console.log(body[0].description);
+      // callback(body[0].description);
+      callback(null, body[0].description);
     }
   });
 };
 
-getContent(url);
+module.exports = { fetchBreedDescription };
+
+// const breedDescription = (breedName) => {
+//   const id = breedName.slice(0, 4);
+//   const url = `https://api.thecatapi.com/v1/breeds/search?q=${id}`;
+//   needle.get(url, (error, respond, body) => {
+//     if (!respond) {
+//       console.error("Request Failed", error);
+//       return;
+//     } else if (body.length === 0) {
+//       console.error("Breed Not Found!!!", error);
+//       return;
+//     } else {
+//       console.log(body[0].description);
+//     }
+//   });
+// };
+
+// breedDescription(process.argv[2]);
+
+// fetchBreedDescription(process.argv[2], (message) => console.log(message));
